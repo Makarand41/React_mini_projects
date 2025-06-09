376267adf5172371df0b379836e5fcdf6ed7fb1d
Patil Makarand <90332486+Makarand41@users.noreply.github.com>
2025-06-09 21:39:37 +0530

    Update App.jsx

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -11,15 +11,23 @@ const decrease1=()=>{
   SetCount(count-1);
 }
 
+const reset1=()=>{
+  SetCount(0);
+}
+
 
 
   return (
 <>
-<button onClick={increase1}>Click me to Increase</button>
-
-<button onClick={decrease1}>Click me to decrease</button>
 
 <h1>{count}</h1>
+<br/>
+<button onClick={increase1}>Increase</button>
+<br/><br/>
+<button onClick={decrease1}>Decrease</button>
+<br/><br/>
+<button onClick={reset1}>Reset</button>
+
 </>
   )
 }
