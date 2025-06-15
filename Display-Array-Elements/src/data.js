beef0f449a3088e0387f2f15edb57cdf46bcc371
Patil Makarand <90332486+Makarand41@users.noreply.github.com>
2025-06-15 23:50:25 +0530

    Update data.js

--- Display-Array-Elements/src/data.js
+++ Display-Array-Elements/src/data.js
@@ -1,12 +1,10 @@
 // data.js
 export const data = [
   {
-    id: 1,
     q: "What is React JS?",
     ans: "React is a frontend JavaScript library used for building user interfaces based on components."
   },
   {
-    id: 2,
     q: "Who developed React JS?",
     ans: "Jordan Walke"
   }
