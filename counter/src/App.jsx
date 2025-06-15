437743df3c693d4f7947bd05e55b77df5ab547db
Patil Makarand <90332486+Makarand41@users.noreply.github.com>
2025-06-15 12:47:03 +0530

    Update App.jsx

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,33 +1,36 @@
-import React, { useState } from 'react'
-export default function App() {
+import { useState } from "react"
 
-  const[count,SetCount]=useState(0)
+const App = () => {
 
-const increase1=()=>{
-    SetCount(count+1);
-}
+  const [count, setCount]=useState(0);
 
-const decrease1=()=>{
-  SetCount(count-1);
-}
+  const increase =()=>  setCount(count+1);
 
-const reset1=()=>{
-  SetCount(0);
-}
+  const decrease =()=> {
+    if (count>0){
+    setCount(count-1);
+    }
+    else{
+      setCount(0);
+    }
+  }
 
-
+  const reset=()=>  setCount(0);
 
   return (
-<>
+    <>
+      <p>{count}</p>
 
-<h1>{count}</h1>
-<br/>
-<button onClick={increase1}>Increase</button>
-<br/><br/>
-<button onClick={decrease1}>Decrease</button>
-<br/><br/>
-<button onClick={reset1}>Reset</button>
+    <button onClick={increase}>Increase</button>
+    <br/><br/>
 
-</>
+   <button onClick={decrease} disabled={count === 0}>Decrease</button>
+
+    <br/><br/>
+    <button onClick={reset}>Reset</button>
+    
+    </>
   )
 }
+
+export default App
